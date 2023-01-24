58b201d0aec59c6023140392598b6b41cfeb07d1
adrian-szwejk <aszwe2@illinois.edu>
2023-01-24 02:57:24 -0600

    got csv formatting and correct opening and closing times connected to current real world time

--- lib/src/screens/restaurant_screen_improved.dart
+++ lib/src/screens/restaurant_screen_improved.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:csv/csv.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 
 //Main widget to run restaurant screen
 class RestaurantScreen extends StatefulWidget {
@@ -104,51 +107,101 @@ class ThirdRoute extends StatelessWidget {
 }
 
 class _MyHomePageState extends State<RestaurantScreen> {
-  // List to keep track of which indices are still in the list view
-  final List<int> _listTileIndices = List.generate(12, (index) => index);
+  List<List<dynamic>> _data = [];
+  var datetime = DateTime.now();
 
-  // Function to remove the list tile at the given index
-  void _removeAtIndex(int listTileIndexToRemove) {
-    // Using setState to tell Flutter to rebuild the widget and UI
+  void _loadCSV() async {
+    final rawData = await rootBundle.loadString("assets/Restaurants.csv");
+    List<List<dynamic>> listData = const CsvToListConverter().convert(rawData);
     setState(() {
-      // Remove the listTileIndex at the given index
-      _listTileIndices.removeAt(listTileIndexToRemove);
+      _data = listData;
     });
   }
+
+  ScrollController scrollController = ScrollController(
+    initialScrollOffset: 10, // or whatever offset you wish
+    keepScrollOffset: true,
+  );
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: const Text("UIUC Eats"),
       ),
-      // Using a ListView.builder widget so that we can use the index of each ListTile being built to customized that
-      // ListTile
       body: ListView.builder(
-        // This anonymous function gives you an updated BuildContext (you don't have to worry about that right now but
-        // you can look up what it is) and the index of the current item being built. It has to return a Widget which
-        // the ListView will use to create its children
-        itemBuilder: (context, listTileIndex) => ListTile(
-          // Widget used to create the circle at the start of the ListTile. Can be used for other things such as
-          leading: CircleAvatar(
-              backgroundColor:
-                  Colors.primaries[listTileIndex % Colors.primaries.length]),
-          // The title text of the ListTile
-          title: const Text('I am a ListTile'),
-          // The text below the title of the ListTile
-          subtitle: Text('Index: ${_listTileIndices[listTileIndex]}'),
-          // The delete button at the end of the ListTile
-          trailing: IconButton(
-            // Call the _removeAtIndex function when button is pressed with the index of the current ListTile
-            onPressed: () {
-              _removeAtIndex(listTileIndex);
-            },
-            icon: const Icon(Icons.delete),
-          ),
-        ),
-        // Tells the ListView how many ListTiles to build
-        itemCount: _listTileIndices.length,
+        controller: scrollController,
+        scrollDirection: Axis.vertical,
+        shrinkWrap: true,
+        itemCount: _data.length,
+        itemBuilder: (_, index) {
+          int curTime = int.parse(datetime.hour.toString());
+          var checkstartTime = _data[index][1]
+              .toString()
+              .split(':')[0]
+              .replaceAll(RegExp(r'[^0-9]'), '');
+          int startTime;
+          if (checkstartTime == "") {
+            startTime = 0;
+          } else {
+            startTime = int.parse(checkstartTime);
+          }
+          var checkcloseTime = _data[index][2]
+              .toString()
+              .split(':')[0]
+              .replaceAll(RegExp(r'[^0-9]'), '');
+          int closeTime;
+          if (checkcloseTime == "") {
+            closeTime = 0;
+          } else {
+            closeTime = int.parse(checkcloseTime);
+          }
+          return Card(
+            margin: const EdgeInsets.all(3),
+            color: index == 0 ? Colors.amber : Colors.white,
+            child: ListTile(
+              title: Text(_data[index][0].toString()),
+              subtitle: Text(_data[index][2].toString()),
+              trailing: Wrap(
+                spacing: 20, // space between two icons
+                children: <Widget>[
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.teal,
+                      // Background color
+                      foregroundColor: Colors.white,
+                      // Text Color (Foreground color)
+                    ),
+                    child: const Text('Visit'),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const SecondRoute()),
+                      );
+                    },
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 13.0),
+                    child: Text(
+                      (curTime < startTime || curTime >= closeTime
+                          ? "Closed"
+                          : "Open"),
+                      style: const TextStyle(
+                          color: Colors.red, fontWeight: FontWeight.bold),
+                    ),
+                  ), // icon-2
+                ],
+              ),
+            ),
+          );
+        },
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _loadCSV,
+        child: const Icon(Icons.refresh),
+      ),
+      // Display the contents from the CSV file
     );
   }
 }
@@ -190,7 +243,7 @@ class _RestaurantScreenState extends State<RestaurantScreen> {
                   Padding(
                     padding: const EdgeInsets.only(top: 13.0),
                     child: Text(
-                      "CLOSED",
+                      "asdf",
                       style: TextStyle(
                           color: Colors.red, fontWeight: FontWeight.bold),
                     ),
